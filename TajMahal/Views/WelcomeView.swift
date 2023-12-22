bf537565bfb7f3b3f0469d4333b5fc4b1123a293
lab@walimaloui.com <lab@walimaloui.com>
2023-12-23 00:15:37 +0100

    Add titles and logo in Stacks

--- TajMahal/Views/WelcomeView.swift
+++ TajMahal/Views/WelcomeView.swift
@@ -12,11 +12,36 @@ struct WelcomeView: View {
     var body: some View {
         NavigationStack {
             Image("TajMahal")
+            HStack {
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("Restaurant Indien")
+                        .fontWeight(.light)
+                        .foregroundStyle(.gray)
+                    Text("Taj Mahal")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                }
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                Spacer()
+            }
+             
             Spacer()
+            
             NavigationLink {
                 MenuView()
             } label : {
-                Text("Menu")
+                Text("Accéder au Menu")
             }
         }
     }
