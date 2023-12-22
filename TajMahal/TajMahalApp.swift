eb6dcb7ff0b45f7a199aefadc31cb4a4058f5f82
lab@walimaloui.com <lab@walimaloui.com>
2023-12-22 21:50:47 +0100

    Initial Commit

---
+++ TajMahal/TajMahalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TajMahalApp.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TajMahalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
