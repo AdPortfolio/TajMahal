60201b6b74ecda69f4b8daa410bfa2d76dc580d6
lab@walimaloui.com <lab@walimaloui.com>
2023-12-22 22:03:28 +0100

    Add MenuView

---
+++ TajMahal/Views/MenuView.swift
@@ -0,0 +1,24 @@
+//
+//  MenuView.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 22/12/2023.
+//
+
+import SwiftUI
+
+// Menu sous forme de liste
+struct MenuView: View {
+    // Référence vers le view model qui permet d'accéder aux tableaux d'entrées et de plats du menu
+    let viewModel: ViewModel = ViewModel()
+    
+    var body: some View {
+        List {
+            // À completer
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
