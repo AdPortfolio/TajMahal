b07ea044987367195795e0507584f5ec4c6f8abe
lab@walimaloui.com <lab@walimaloui.com>
2023-12-22 22:02:42 +0100

    Add Model

---
+++ TajMahal/Model.swift
@@ -0,0 +1,26 @@
+//
+//  Model.swift
+//  TajMahal
+//
+//  Created by Walim Aloui on 22/12/2023.
+//
+
+import Foundation
+
+// Cette enumération répertorie 3 niveaux de piment
+enum SpiceLevel {
+    case light
+    case medium
+    case hot
+}
+
+// Représente l'objet "plat", qui figure sur la carte du menu
+struct Dish {
+    var name: String
+    var description: String
+    var allergens: String
+    var ingredients: String
+    var spiceLevel: SpiceLevel
+    var imageName: String
+}
+
