02f86ea9e57ff458df71a7cb2b251d68a316fded
lab@walimaloui.com <lab@walimaloui.com>
2023-12-23 20:00:38 +0100

    Finish contructing WelcomView

--- TajMahal/Views/WelcomeView.swift
+++ TajMahal/Views/WelcomeView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 // Page d'accueil
 struct WelcomeView: View {
+    
+    let viewModel = ViewModel()
+    
     var body: some View {
         NavigationStack {
             Image("TajMahal")
+                .resizable()
+                .padding(.leading)
+                .padding(.trailing)
+            
             HStack {
-                Spacer()
+                
                 VStack(alignment: .leading) {
                     Text("Restaurant Indien")
                         .fontWeight(.light)
@@ -22,27 +29,123 @@ struct WelcomeView: View {
                         .font(.title2)
                         .fontWeight(.bold)
                 }
+                .padding(.leading)
                 
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
                 Spacer()
                 
                 Image("Logo")
                     .resizable()
+                    .foregroundColor(Color.gray)
                     .frame(width: 40, height: 40)
-                Spacer()
+                    .padding(.trailing)
             }
-             
+            
             Spacer()
             
-            NavigationLink {
-                MenuView()
-            } label : {
-                Text("Accéder au Menu")
+            VStack(alignment: .center, spacing: 15) {
+                
+                HStack {
+                    Image("clock")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gray)
+                        .frame(width: 20, height: 17)
+                        .padding(.leading)
+                    Text("Mardi")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                        .padding(.trailing)
+                        .padding(.trailing)
+                    Spacer()
+                    Spacer()
+                    Text("11h30 - 14h30 · 18h30 - 22h00")
+                        .minimumScaleFactor(0.5)
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                        .padding(.trailing)
+                }
+                
+                // MARK: - 2
+                HStack {
+                    Image("cookingPot")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gray)
+                        .frame(width: 20, height: 17)
+                        .padding(.leading)
+                    
+                    Text("Type de Service")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                        .padding(.trailing)
+                        .padding(.trailing)
+                    Spacer()
+                    
+                    Text("À emporter")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                        .padding(.trailing)
+                }
+                
+                // MARK: - 3
+                HStack {
+                    Image("location")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gray)
+                        .frame(width: 20, height: 17)
+                        .padding(.leading)
+                    
+                    Text("12 Avenue de la Brique - 75010 Paris")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                        .padding(.trailing)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.9)
+                    Spacer()
+                }
+                
+                // MARK: - 4
+                HStack {
+                    Image("globe")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gray)
+                        .frame(width: 20, height: 17)
+                        .padding(.leading)
+                    Text("www.tajmahal.fr")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                
+                // MARK: - 5
+                HStack {
+                    Image("phone")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.gray)
+                        .frame(width: 20, height: 17)
+                        .padding(.leading)
+                    
+                    Text("06 12 34 56 78")
+                        .foregroundStyle(.gray)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
             }
+            
+            NavigationLink(destination: MenuView()) {
+                Text("Accéder au menu")
+                    .fontWeight(.bold)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .foregroundStyle(Color.white)
+                    .padding(10)
+                    .background(Color("CustomRed"))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .padding()
         }
     }
 }
