65a2a33a82420f8fe9f0c69a438ec1d87f5059f1
lab@walimaloui.com <lab@walimaloui.com>
2024-01-08 17:33:19 +0100

    Add customFonts and customGray methods to extension

--- TajMahal/SwiftUI Extensions/Text+CustomFonts.swift
+++ TajMahal/SwiftUI Extensions/Text+CustomFonts.swift
@@ -11,5 +11,17 @@ extension Text {
     func customPlusJakartaSansMedium(size: CGFloat) -> Text {
         return self.font(Font.custom("PlusJakartaSans-Medium", size: size))
     }
+    
+    func customPlusJakartaSansSemiBold(size: CGFloat) -> Text {
+        return self.font(Font.custom("PlusJakartaSans-SemiBold", size: size))
+    }
+    
+    func customPlusJakartaSansSemiBoldItalic(size: CGFloat) -> Text {
+        return self.font(Font.custom("PlusJakartaSans-SemiBoldItalic", size: size))
+    }
+    
+    func customGray() -> Text {
+        return self.foregroundStyle(Color(red: 102/255, green: 102/255, blue: 102/255))
+    }
 }
 
