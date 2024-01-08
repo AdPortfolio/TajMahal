4626ceb97bc06ba5904168c18aea4bbe429e77ff
lab@walimaloui.com <lab@walimaloui.com>
2024-01-08 22:55:04 +0100

    Add Custom Fonts

--- TajMahal/SwiftUI Extensions/Text+CustomFonts.swift
+++ TajMahal/SwiftUI Extensions/Text+CustomFonts.swift
@@ -23,5 +23,9 @@ extension Text {
     func customGray() -> Text {
         return self.foregroundStyle(Color(red: 102/255, green: 102/255, blue: 102/255))
     }
+    
+    func menuGray() -> Text {
+        return self.foregroundStyle(Color(red: 51/255, green: 51/255, blue: 51/255))
+    }
 }
 
